fd4e8428ef7b2252d79e905449737bb0c3fa1c24
Yuri Correa <yurifcorrea@gmail.com>
2021-11-04 09:46:15 -0300

    commit 6: FilterEmployees route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { CreateEmployeeController } from "./controllers/CreateEmployeeController"
 import { DeleteEmployeeController } from "./controllers/DeleteEmployeeController"
+import { FilterEmployeesController } from "./controllers/FilterEmployeesController"
 import { ListEmployeesController } from "./controllers/ListEmployeesController"
 import { UpdateEmployeeController } from "./controllers/UpdateEmployeeController"
 
@@ -10,10 +11,12 @@ const createEmployeeController = new CreateEmployeeController()
 const listEmployeesController = new ListEmployeesController()
 const updateEmployeeController = new UpdateEmployeeController()
 const deleteEmployeeController = new DeleteEmployeeController()
+const filterEmployeesController = new FilterEmployeesController()
 
 router.post("/employee", createEmployeeController.handle)
 
 router.get("/list", listEmployeesController.handle)
+router.get("/filter", filterEmployeesController.handle)
 
 router.put("/update", updateEmployeeController.handle)
 
